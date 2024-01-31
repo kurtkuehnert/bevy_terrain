5620a1c2971c19b891e456df449bee3b01052215
Kurt Kühnert <kurt@kuehnert.dev>
2024-01-31 14:17:37 +0100

    rebased onto latest bevy version cleaned up plugin

--- examples/planar/preprocess_planar.rs
+++ examples/planar/preprocess_planar.rs
@@ -37,6 +37,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let mut preprocessor = Preprocessor::new(PATH.to_string());
 
+    preprocessor.clear_attachment(0, &mut terrain_bundle.node_atlas);
+    preprocessor.clear_attachment(1, &mut terrain_bundle.node_atlas);
     preprocessor.preprocess_tile(
         PreprocessDataset {
             attachment_index: 0,
--- examples/spherical/preprocess_spherical.rs
+++ examples/spherical/preprocess_spherical.rs
@@ -26,20 +26,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         format: AttachmentFormat::R16,
         ..default()
     });
-    //.add_attachment(AttachmentConfig {
-    //   name: "height2".to_string(),
-    //   texture_size: TEXTURE_SIZE,
-    //   border_size: 2,
-    //   format: AttachmentFormat::R16,
-    //   ..default()
-    //);
 
     let mut terrain_bundle = TerrainBundle::new(config, Vec3::ZERO, 0.0);
 
     let mut preprocessor = Preprocessor::new(PATH.to_string());
 
     preprocessor.clear_attachment(0, &mut terrain_bundle.node_atlas);
-
     preprocessor.preprocess_spherical(
         PreprocessDataset {
             attachment_index: 0,
@@ -49,32 +41,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         &asset_server,
         &mut terrain_bundle.node_atlas,
     );
-
-    // for side in 0..6 {
-    //     preprocessor.preprocess_tile(
-    //         PreprocessDataset {
-    //             attachment_index: 1,
-    //             path: "terrains/spherical/source/height/200m.tif".to_string(),
-    //             side,
-    //             top_left: Vec2::new(0.0, 0.0),
-    //             bottom_right: Vec2::new(0.0, 0.0),
-    //         },
-    //         &asset_server,
-    //         &mut terrain_bundle.node_atlas,
-    //     );
-    // }
-    //
-    // preprocessor.preprocess_tile(
-    //     PreprocessDataset {
-    //         attachment_index: 1,
-    //         path: "terrains/spherical/source/height/200m.tif".to_string(),
-    //         side: 2,
-    //         top_left: Vec2::new(0.2077404, 0.4357290),
-    //         bottom_right: Vec2::new(0.3284694, 0.5636175),
-    //     },
-    //     &asset_server,
-    //     &mut terrain_bundle.node_atlas,
-    // );
 
     commands.spawn((terrain_bundle, preprocessor));
 }
