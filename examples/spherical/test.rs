d7623242237135788184d954a8bc674ebdcba093
Kurt Kühnert <kurt@kuehnert.dev>
2024-03-04 14:03:48 +0100

    added test example

--- examples/spherical/test.rs
+++ examples/spherical/test.rs
@@ -1,13 +1,11 @@
 use bevy::{prelude::*, reflect::TypePath, render::render_resource::*};
 use bevy_terrain::prelude::*;
 
-const PATH: &str = "terrains/spherical";
 const RADIUS: f32 = 50.0;
 const MIN_HEIGHT: f32 = -12.0 / 6371.0;
 const MAX_HEIGHT: f32 = 9.0 / 6371.0;
 const SUPER_ELEVATION: f32 = 10.0;
 const TEXTURE_SIZE: u32 = 512;
-const LOD_COUNT: u32 = 5;
 
 #[derive(Asset, AsBindGroup, TypePath, Clone)]
 pub struct TerrainMaterial {
@@ -49,19 +47,26 @@ fn setup(
     mut view_configs: ResMut<TerrainViewComponents<TerrainViewConfig>>,
     asset_server: Res<AssetServer>,
 ) {
-    let gradient = asset_server.load("textures/gradient1.png");
+    let gradient1 = asset_server.load("textures/gradient1.png");
     images.load_image(
-        &gradient,
+        &gradient1,
+        TextureDimension::D1,
+        TextureFormat::Rgba8UnormSrgb,
+    );
+
+    let gradient2 = asset_server.load("textures/gradient2.png");
+    images.load_image(
+        &gradient2,
         TextureDimension::D1,
         TextureFormat::Rgba8UnormSrgb,
     );
 
     // Configure all the important properties of the terrain, as well as its attachments.
-    let config = TerrainConfig {
-        lod_count: LOD_COUNT,
+    let config1 = TerrainConfig {
+        lod_count: 5,
         min_height: MIN_HEIGHT * SUPER_ELEVATION,
         max_height: MAX_HEIGHT * SUPER_ELEVATION,
-        path: PATH.to_string(),
+        path: "terrains/spherical".to_string(),
         ..default()
     }
         .add_attachment(AttachmentConfig {
@@ -72,11 +77,37 @@ fn setup(
             format: AttachmentFormat::R16,
         });
 
-    let terrain = commands
+    // let terrain1 = commands
+    //     .spawn((
+    //         TerrainBundle::new(config1.clone(), default(), RADIUS),
+    //         materials.add(TerrainMaterial {
+    //             gradient: gradient1,
+    //             super_elevation: SUPER_ELEVATION,
+    //         }),
+    //     ))
+    //     .id();
+
+    // Configure all the important properties of the terrain, as well as its attachments.
+    let config2 = TerrainConfig {
+        lod_count: 8,
+        min_height: MIN_HEIGHT * SUPER_ELEVATION,
+        max_height: MAX_HEIGHT * SUPER_ELEVATION,
+        path: "terrains/test".to_string(),
+        ..default()
+    }
+        .add_attachment(AttachmentConfig {
+            name: "height".to_string(),
+            texture_size: TEXTURE_SIZE,
+            border_size: 2,
+            mip_level_count: 4,
+            format: AttachmentFormat::R16,
+        });
+
+    let terrain2 = commands
         .spawn((
-            TerrainBundle::new(config.clone(), Vec3::new(100.0, 100.0, 100.0), RADIUS),
+            TerrainBundle::new(config2.clone(), default(), RADIUS),
             materials.add(TerrainMaterial {
-                gradient: gradient.clone(),
+                gradient: gradient2,
                 super_elevation: SUPER_ELEVATION,
             }),
         ))
@@ -84,13 +115,21 @@ fn setup(
 
     // Configure the quality settings of the terrain view. Adapt the settings to your liking.
     let view_config = TerrainViewConfig::default();
-
     let view = commands.spawn((TerrainView, DebugCamera::default())).id();
 
+    // initialize_terrain_view(
+    //     terrain1,
+    //     view,
+    //     &config1,
+    //     view_config.clone(),
+    //     &mut quadtrees,
+    //     &mut view_configs,
+    // );
+
     initialize_terrain_view(
-        terrain,
+        terrain2,
         view,
-        &config,
+        &config2,
         view_config,
         &mut quadtrees,
         &mut view_configs,
